963274b7bb3a7b699005ec72494c7c80746fa7c5
LeoGCode <89754962+LeoGCode@users.noreply.github.com>
2022-06-11 22:10:43 -0400

    [FIX] commets

--- ProgramSimulator.py
+++ ProgramSimulator.py
@@ -1,29 +1,33 @@
 # Implementation of a simulator of programs, interpreters and translators based on Tombstone diagrams
+#
 # This program has the following characteristics:
 # (a) It's able to handle programs, interpreters and translators, these can be.
-# - PROGRAM <name> <language>.
-# Represents a program identified by <name> written in <language>.
-# - INTERPRETER <base_language> <language>
-# Represents an interpreter for <language> written in <base_language>.
-# - TRANSLATOR <base_language> <source_language> <target_language>.
-# Represents a translator, from <origin_language> to <target_language>, written in <base_language>.
+#       - PROGRAM <name> <language>.
+#           Represents a program identified by <name> written in <language>.
+#
+#       - INTERPRETER <base_language> <language>
+#           Represents an interpreter for <language> written in <base_language>.
+#
+#       - TRANSLATOR <base_language> <source_language> <target_language>.
+#           Represents a translator, from <origin_language> to <target_language>, written in <base_language>.
+#
 # (b) All languages must be alphanumeric strings.
 # (c) There shall be a special name LOCAL which shall refer to the language that can be interpreted by the local
 # machine. the local machine can interpret.
 # (d) Once the program has started, it will repeatedly prompt the user for an action to proceed. Such action may be:
 #
-# i. DEFINE <type> [<arguments>].
+#       i. DEFINE <type> [<arguments>].
 # Represents a class definition <type> with <arguments> (see above for the types and arguments to be supported).
 # The program should report an error and ignore the action if <name> already has an associated program, in the
 # case of programs.
 #
-# ii. EXECUTABLE <name>
+#       ii. EXECUTABLE <name>
 # Represents a query for the possibility of executing the program of name <name>. Your program must print whether
 # it is possible to construct what is requested, using only the definitions made so far. The program should report an
 # error and ignore the action if <name> does not have an
 # associated program.
 #
-# iii. EXIT
+#       iii. EXIT
 # You must exit the simulator.
 #
 # At the end of the execution of each action, the program shall prompt to the user for the next action.
